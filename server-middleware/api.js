e7121c1ebf4dc7c864604c72017f41eb2f4ea1f4
marnixah <marnix@hagehoeve.com>
2021-05-20 22:55:37 +0200

    feat(api): changed getting timeslots to db

--- server-middleware/api.js
+++ server-middleware/api.js
@@ -18,15 +18,17 @@ assert(dbpassword, "Please specify a value for DB_PASSWORD")
 assert(dbuser, "Please specify a value for DB_USER")
 
 const dbURL = `mongodb://${dbhost}:27017/${dbname}`
-mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(dbURL, {
+  auth: { "authSource": "admin" },
+  useNewUrlParser: true, useUnifiedTopology: true,
+  user: dbuser,
+  pass: dbpassword
+})
   .then((result) => console.log("\x1b[32m✔\x1b[0m Connected to db"))
   .catch((err) => console.error(err))
 
-app.all('/getTimeslots', (req, res) => {
-  let timeslots = [
-    {startTimeUnix: 1621497559886, endTimeUnix: 1621591181598, people: [{name: "marnix", picture: "https://media.discordapp.net/attachments/713394836568277008/816086489465094204/unknown.png"}]},
-    {startTimeUnix: 1621497559887, endTimeUnix: 1621591181599, people: [{name: "marnix", picture: "https://media.discordapp.net/attachments/713394836568277008/816086489465094204/unknown.png"}]}
-  ]
+app.all('/getTimeslots', async (req, res) => {
+  timeslots = await Timeslot.find()
   res.send(JSON.stringify(timeslots))
 })
 
