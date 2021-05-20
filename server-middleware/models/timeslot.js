a870e25885fcdc7c8c7aba4a9b68703b6102917c
marnixah <marnix@hagehoeve.com>
2021-05-20 19:06:08 +0200

    feat: added mongoose

---
+++ server-middleware/models/timeslot.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const timeslotSchema = new Schema({
+  startTimeUnix: {
+    type: Number,
+    required: true
+  },
+  endTimeUnix: {
+    type: Number,
+    required: false
+  },
+  family: {
+    type: [{
+      name: String,
+      picture: String,
+      amountOfPeople: Number
+    }]
+  }
+})
+
+const Timeslot = mongoose.model('Timeslot', timeslotSchema)
+module.exports = Timeslot
